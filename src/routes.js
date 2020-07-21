63975290c8b772e76a0477514a99178179d2f285
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-07-21 18:02:35 -0300

    Read from CRUD created

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,10 @@ routes.get('/', function(req, res){
 })
 
 routes.get('/products/create', ProductController.create)
+routes.post('/products', ProductController.post)
+
+
+
 
 routes.get('/ads/create', function(req, res){
     return res.redirect("/products/create")
