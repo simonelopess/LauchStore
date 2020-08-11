f7e06ddeb70761cb07cb3b317ad21e112abd0aba
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-08-11 09:42:35 -0300

    Finalizado o gerenciador de de upload de fotos

--- public/scripts.js
+++ public/scripts.js
@@ -18,15 +18,19 @@ const Mask = {
 }
 
 const PhotosUpload = {
+    input: "", 
     preview: document.querySelector('#photos-preview'),
     uploadLimit: 6,
+    files: [],
     handleFileInput(event){
 
         const { files: fileList} = event.target
-      
+        PhotosUpload.input = event.target
+
         if(PhotosUpload.hasLimit(event)) return 
         Array.from(fileList).forEach(file => {
             
+            PhotosUpload.files.push(file)
             const reader = new FileReader()
 
             reader.onload = () => {
@@ -41,17 +45,42 @@ const PhotosUpload = {
 
             reader.readAsDataURL(file)
         })
+
+       PhotosUpload.input.files= PhotosUpload.getAllFiles()
     },
     hasLimit(event){
-        const {uploadLimit} = PhotosUpload
-        const {files: fileList} = event.target
-
+        const {uploadLimit, input, preview} = PhotosUpload
+        const { files : fileList} = input
+        
         if (fileList.length > uploadLimit){
             alert(`Envie no máximo ${uploadLimit} fotos`)
             event.preventDefault()
             return true
         }
+        
+        const photosDiv = []
+        preview.childNodes.forEach(item => {
+            if(item.classList && item.classList.value == "photo")
+                photosDiv.push(item)
+        })
+
+        const totalPhotos = fileList.length + photosDiv.length
+
+        if(totalPhotos  > uploadLimit){
+            alert('Você atingiu o limite máximo de fotos ')
+            event.preventDefault()
+
+            return true
+        }
+
         return false;
+    },
+    getAllFiles(){
+        const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+
+        PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
+
+        return dataTransfer.files
     },
     getContainer(image){
         const div = document.createElement('div')
@@ -74,6 +103,8 @@ const PhotosUpload = {
         const photoDiv = event.target.parentNode
         const photosArray = Array.from(PhotosUpload.preview.children)
         const index = photosArray.indexOf(photoDiv)
+        PhotosUpload.files.splice(index, 1)
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
 
         photoDiv.remove();
     }
