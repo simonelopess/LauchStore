69c2c40ca42ca1fa4b0c357cda33221453a44f08
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-08-10 22:05:31 -0300

    criação do butão excluir foto e estilização

--- public/scripts.js
+++ public/scripts.js
@@ -18,37 +18,56 @@ const Mask = {
 }
 
 const PhotosUpload = {
+    preview: document.querySelector('#photos-preview'),
     uploadLimit: 6,
     handleFileInput(event){
 
         const { files: fileList} = event.target
-        const {uploadLimit} = PhotosUpload
-
-        if (fileList.length > uploadLimit){
-            alert(`Envie no máximo ${uploadLimit} fotos`)
-            event.preventDefault()
-            return 
-        }
-
+      
+        if(PhotosUpload.hasLimit(event)) return 
         Array.from(fileList).forEach(file => {
+            
             const reader = new FileReader()
 
             reader.onload = () => {
               const image = new Image()
               image.src = String(reader.result)      
 
-              const div = document.createElement('div')
+              const div = PhotosUpload.getContainer(image)
+
+              PhotosUpload.preview.appendChild(div)
+
+            }
+
+            reader.readAsDataURL(file)
+        })
+    },
+    hasLimit(event){
+        const {uploadLimit} = PhotosUpload
+        const {files: fileList} = event.target
+
+        if (fileList.length > uploadLimit){
+            alert(`Envie no máximo ${uploadLimit} fotos`)
+            event.preventDefault()
+            return true
+        }
+        return false;
+    },
+    getContainer(image){
+        const div = document.createElement('div')
               div.classList.add('photo')
 
               div.onclick = () => alert('remover foto')
 
               div.appendChild(image)
 
-              document.querySelector('#photos-preview').appendChild(div)
-
-            }
-
-            reader.readAsDataURL(file)
-        })
+              div.appendChild(PhotosUpload.getRemoveButton());
+              return div
+    },
+    getRemoveButton(){
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = 'close'
+        return button
     }
 }
