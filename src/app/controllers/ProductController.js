150c3e748f0bb3c826a8d89c0e21c64ac0d123bc
Simone Lopes da Silva <simlsilva@firjan.com.br>
2020-08-11 22:08:45 -0300

    Apresentando imagem no gerenciador de produtos

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -35,6 +35,8 @@ module.exports = {
     }))
 
       await Promise.all(filesPromise)
+      
+
 
     return res.redirect(`/products/${productId}/edit`);
   },
@@ -48,10 +50,19 @@ module.exports = {
     product.old_price = formatPrice(product.old_price);
     product.price = formatPrice(product.price);
 
+    //get categories
     results = await Category.all();
     const categories = results.rows;
 
-    return res.render('products/edit.njk', { product, categories });
+    //get images
+    results = await Product.files(product.id)
+    let files = results.rows
+    files = files.map(file => ({
+      ...file,  
+      src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
+    }))
+
+    return res.render('products/edit.njk', { product, categories, files });
   },
 
   async put(req, res) {
